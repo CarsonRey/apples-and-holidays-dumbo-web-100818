6c7f883ec139a1555c5f6a8b793d20ba131a9806
Carson Rey <c.rey494@gmail.com>
2018-09-21 16:46:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_array = nil
-  holiday_hash.select do |season, holidays|
+  holiday_hash.each do |season, holidays|
     holidays
 end
 end
