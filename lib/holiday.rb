6b8576eb59ab1ddeaf3a6f26f131cc82a0c93187
Carson Rey <c.rey494@gmail.com>
2018-09-21 15:10:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.select do |season, holiday|
     
-    holiday.has_value?("BBQ")
+    holiday.key("BBQ")
       
   end
 end
