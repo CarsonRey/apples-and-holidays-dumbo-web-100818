56c811b295f3bcdcabc9030d26122d922e1b0d8c
Carson Rey <c.rey494@gmail.com>
2018-09-21 19:02:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,6 +78,7 @@ def all_supplies_in_holidays(holiday_hash)
   "#{season}: #{holiday}:"
 end
 end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
