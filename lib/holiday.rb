b60a64a87aae3e00c7d8ba9e0c17ace1eaf08d28
Carson Rey <c.rey494@gmail.com>
2018-09-21 20:52:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
         words.capitalize!
       end
       i += 1
-      celebration.
+      
       items.each do |x|
         supply << x.join(" ")
         supply
