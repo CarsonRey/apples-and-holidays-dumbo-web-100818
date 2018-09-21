7bbbc16cfc79374dfcab59b0311fba913c4a09f9
Carson Rey <c.rey494@gmail.com>
2018-09-21 21:46:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,16 +69,13 @@ def all_supplies_in_holidays(holiday_hash)
           words.capitalize!
         end
         celebration = celebration.join(" ")
-        
-      
        items = items.join(", ")
        puts "  #{celebration}: #{items}"
       end
     end
-
 end
 
-
+#
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
