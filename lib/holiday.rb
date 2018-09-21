f7574a0493aa1ff5b1739e1d331c0d7b0bea594d
Carson Rey <c.rey494@gmail.com>
2018-09-21 21:38:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,10 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize!}:"
       data.each do |holiday, items|
       celebration = holiday.to_s.split("_")
-      
+      day = []
+        celebration.collect do |day|
+          day.capitalize!
+        end
       items.each do |x|
        supply << x.join(", ")
       end
