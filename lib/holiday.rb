47f3257a5530cc63ac75d0b4c7a77dafea37300d
Carson Rey <c.rey494@gmail.com>
2018-09-21 18:09:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   hash.each do |season, holidays|
-    
+    puts season.to_s.capitalize + ":"
   end
+  
 end
 
 def all_holidays_with_bbq(holiday_hash)
